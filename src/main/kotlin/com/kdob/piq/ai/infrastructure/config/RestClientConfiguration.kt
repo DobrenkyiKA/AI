35f29f572ec4fb1fcc37455eccf72d30ab303364
Konstantin Dobrenkii <6092822@gmail.com>
2026-03-08 18:33:22 +0400

    Add JWT token to request storage service

--- src/main/kotlin/com/kdob/piq/ai/infrastructure/config/RestClientConfiguration.kt
+++ src/main/kotlin/com/kdob/piq/ai/infrastructure/config/RestClientConfiguration.kt
@@ -2,6 +2,8 @@ package com.kdob.piq.ai.infrastructure.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.web.client.RestClient
 
 @Configuration
@@ -11,5 +13,13 @@ class RestClientConfig {
     fun questionServiceRestClient(): RestClient =
         RestClient.builder()
             .baseUrl("http://localhost:8082")
+            .requestInterceptor { request, body, execution ->
+                val authentication = SecurityContextHolder.getContext().authentication
+                if (authentication is JwtAuthenticationToken) {
+                    val tokenValue = authentication.token.tokenValue
+                    request.headers.add("Authorization", "Bearer $tokenValue")
+                }
+                execution.execute(request, body)
+            }
             .build()
 }
